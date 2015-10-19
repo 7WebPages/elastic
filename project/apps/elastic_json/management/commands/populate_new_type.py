ceaf5e67b7fe502217dbf822ac5aebe95e987c34
nik <khlestovnikita@gmail.com>
2015-10-19 21:40:51 +0300

    switch to helpers

--- project/apps/elastic_json/management/commands/populate_new_type.py
+++ project/apps/elastic_json/management/commands/populate_new_type.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
-import json
-import requests
 from elastic_json.models import Student
+from elasticsearch.helpers import bulk
+from elasticsearch import Elasticsearch
 
 
 class Command(BaseCommand):
@@ -12,13 +12,9 @@ class Command(BaseCommand):
 
 
 def put_all_to_index(model_name):
-    url = 'http://localhost:9200/_bulk'
+    es = Elasticsearch()
     data = [convert_for_bulk(s, 'create') for s in model_name.objects.all()]
-    data_serialized = '%s\n' % '\n'.join(data)
-    resp = requests.post(url, data=data_serialized)
-    resp_data = resp.json()
-    assert resp_data['errors'] is False
-    assert len(resp_data)['items'] == len(data)
+    bulk(client=es, actions=data, stats_only=True)
 
 
 def convert_for_bulk(django_object, action=None):
@@ -26,12 +22,9 @@ def convert_for_bulk(django_object, action=None):
         raise AttributeError('no action specified')
     data = django_object.es_repr()
     metadata = {
-        action: {
-            "_index": django_object._meta.es_index_name,
-            "_type": django_object._meta.es_type_name,
-            "_id": data['_id']
-        }
+        '_op_type': action,
+        "_index": django_object._meta.es_index_name,
+        "_type": django_object._meta.es_type_name,
     }
-    return '\n'.join(
-        (json.dumps(metadata), json.dumps(data))
-    )
+    data.update(**metadata)
+    return data
