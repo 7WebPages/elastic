cebc34f41e190512af22be978306a1a824e02084
nik <khlestovnikita@gmail.com>
2015-10-20 23:02:57 +0300

    layout ok at last

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,7 @@ if settings.DEBUG:
     try:
         from django.conf.urls.static import static
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
         import debug_toolbar
         urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
