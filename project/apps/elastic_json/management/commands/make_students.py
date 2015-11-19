0eb3ca27abdb77062cba2942563cbffd4753e4eb
Nikita Khlestov <khlestovnikita@gmail.com>
2015-11-19 18:10:52 +0700

    some fixes for a command to create testing data

--- project/apps/elastic_json/management/commands/make_students.py
+++ project/apps/elastic_json/management/commands/make_students.py
@@ -12,6 +12,9 @@ from elastic_json.utils.bulk import put_all_to_index
 class Command(BaseCommand):
     help = "generates dummy data for testing/show purposes."
 
+    def add_arguments(self, parser):
+        parser.add_argument('count', nargs=1, type=int)
+
     def handle(self, *args, **options):
         Student.objects.all().delete()
         University.objects.all().delete()
@@ -20,7 +23,7 @@ class Command(BaseCommand):
         # database part
         # make some Universities
         university_names = (
-            'MIT', 'KNU', 'MGU', 'DonNU', 'CalTech', 'KPI', 'KhPI', 'KhNU', 'DPI', 'PGTU'
+            'MIT', 'MGU', 'CalTech', 'KPI', 'DPI', 'PSTU'
         )
         universities = []
         for name in university_names:
@@ -29,13 +32,13 @@ class Command(BaseCommand):
         # make some courses
         template_options = ['CS%s0%s', 'MATH%s0%s', 'CHEM%s0%s', 'PHYS%s0%s']
         courses = []
-        for num in range(1, 10):
-            for course_num in range(1, 5):
+        for num in range(1, 4):
+            for course_num in range(1, 4):
                 for template in template_options:
                     name = template % (course_num, num)
                     course = mommy.make(Course, name=name)
                     courses.append(course)
-        for _ in xrange(100):
+        for _ in xrange(options.get('count')[0]):
             stud = mommy.make(
                 Student,
                 university=random.choice(universities),
@@ -43,8 +46,8 @@ class Command(BaseCommand):
                 last_name=names.get_last_name(),
                 age=random.randint(17, 25)
             )
-            for _ in range(random.randint(1, 10)):
-                index = random.randint(0, 143)
+            for _ in range(random.randint(1, 4)):
+                index = random.randint(0, len(courses)-1)
                 stud.courses.add(courses[index])
 
         # recreate index
--- project/apps/elastic_json/utils/infrastructure.py
+++ project/apps/elastic_json/utils/infrastructure.py
@@ -2,11 +2,21 @@
 We store abstractions to control infrastructure here.
 '''
 from elasticsearch import Elasticsearch
+client = Elasticsearch()
 
 
 def create_index(index_name='django'):
     '''
-    A command to make an index
+    A command to make an index.
+    Does not raise an exception in case index already exists.
     '''
-    client = Elasticsearch()
-    client.indices.create(index=index_name, ignore=400)
+    client.indices.create(
+        index=index_name,
+        ignore=400,
+        timeout=60,
+        master_timeout=15
+    )
+
+
+def delete(index_name):
+    client.indices.delete(index=index_name)
