2de14c730c41ed2495efcbec883d684e17418ce1
Nikita Khlestov <khlestovnikita@gmail.com>
2015-11-22 12:13:32 +0700

    update

--- project/apps/elastic_json/management/commands/gen_index.py
+++ project/apps/elastic_json/management/commands/gen_index.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 import json
 import requests
 from elastic_json.models import Student
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -12,6 +13,7 @@ class Command(BaseCommand):
         index = Student._meta.es_index_name
         type_name = Student._meta.es_type_name
         # we have to take "method" fields from mapping, as it's internal word
-        url = 'http://localhost:9200/%s/_mapping/%s' % (index, type_name)
+        base_url = settings.ES_URL
+        url = base_url + '%s/_mapping/%s' % (index, type_name)
         resp = requests.put(url, data=json.dumps(mapping))
         assert resp.text == '{"acknowledged":true}'
