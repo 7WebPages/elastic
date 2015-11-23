65957f3b2fd241c34d3737b3e4df21e30149d424
Nikita Khlestov <khlestovnikita@gmail.com>
2015-11-23 15:37:13 +0700

    got to fix it

--- project/settings/base.py
+++ project/settings/base.py
@@ -46,7 +46,10 @@ DATABASES['default'] = dj_database_url.config()
 
 ########## ELASTICSEARCH CONFIGURATION
 from elasticsearch import Elasticsearch, RequestsHttpConnection
+from urlparse import urlparse
 ES_URL = os.environ.get('SEARCHBOX_URL') or 'http://127.0.0.1:9200/'
+if not urlparse(ES_URL).port:
+    ES_URL += ':80'
 ES_CLIENT = Elasticsearch([ES_URL], connection_class=RequestsHttpConnection)
 ########## END ELASTICSEARCH CONFIGURATION
 
